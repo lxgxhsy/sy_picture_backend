e0d651300d2eec0a1cab43fee95353643975a89a
sy <3078567417@qq.com>
2024-12-08 21:42:41 +0800

    初始化项目

---
+++ src/test/java/com/example/sypicturebackend/SyPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sypicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyPictureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
