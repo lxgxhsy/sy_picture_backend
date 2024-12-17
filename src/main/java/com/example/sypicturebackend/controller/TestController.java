3d1d47e8c0d8855e68d1db1bf524226b0899a4b6
sy <3078567417@qq.com>
2024-12-17 16:23:19 +0800

    "项目初始化 全局配置"

---
+++ src/main/java/com/example/sypicturebackend/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.sypicturebackend.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: shiyong
+ * @CreateTime: 2024-12-17
+ * @Description: 测试u
+ * @Version: 1.0
+ */
+
+@Slf4j
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public void test(){
+		System.out.println("dsd");
+	}
+}
